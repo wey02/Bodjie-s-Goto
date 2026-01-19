91fbb18bd98e1a2d06500672ff1abefe2701f1dd
Jewelle <vergarajewelle02@gmail.com>
2026-01-20 01:31:32 +0800

    updated UI

--- animation.js
+++ animation.js
@@ -1,12 +1,11 @@
 
-
-const bgAnimation = document.getElementById('bgAnimation');
-
+const bgAnimations = document.querySelectorAll('.bgAnimation');
 const numberOfColorBoxes = 400;
 
-for (let i = 0; i < numberOfColorBoxes; i++) {
-    const colorBox = document.createElement('div');
-    colorBox.classList.add('colorBox');
-    bgAnimation.append(colorBox)
-}
-
+bgAnimations.forEach(bgAnimation => {
+    for (let i = 0; i < numberOfColorBoxes; i++) {
+        const colorBox = document.createElement('div');
+        colorBox.classList.add('colorBox');
+        bgAnimation.appendChild(colorBox);
+    }
+});
